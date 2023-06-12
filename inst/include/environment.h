d69ab66f7a18d5ffc0344808a4464a5fabe13967
Jaideep <jaideep777@gmail.com>
2023-06-12 19:13:27 +0200

    [feature_pm_et] default value of v_wind through overloaded constructors in ParEnv

--- inst/include/environment.h
+++ inst/include/environment.h
@@ -1,6 +1,7 @@
 #ifndef PHYDRO_ENVIRONMENT_H
 #define PHYDRO_ENVIRONMENT_H
 
+#include <iostream>
 #include "temperature_dependencies_physical.h"
 
 namespace phydro{
@@ -29,14 +30,17 @@ class ParEnv{
 	GsMethod gs_method = GS_IGF;
 	ETMethod et_method = ET_DIFFUSION;
 
-	ParEnv(double _tc, double _patm, double _vpd, double _Rn){
+	ParEnv(double _tc, double _patm, double _vpd, double _Rn, double _v_wind){
 		tc = _tc;
 		vpd = _vpd;
 		patm = _patm;
 		Rn = _Rn;
-		v_wind = 3; // global average value
+		v_wind = _v_wind; 
 
 		calc_temp_dependencies();
+	}
+
+	ParEnv(double _tc, double _patm, double _vpd, double _Rn) : ParEnv(_tc, _patm, _vpd, _Rn, 3) { // global average value of v_wind
 	}
 
 	void calc_temp_dependencies(){
@@ -49,6 +53,22 @@ class ParEnv{
 		epsilon = calc_sat_slope(tc) / gamma;
 
 		lv = calc_enthalpy_vap(tc);
+	}
+
+	void print(){
+		std::cout << "Env:\n";
+		std::cout << "   tc = " << tc << "\n";     // Temperature [degC]
+		std::cout << "   patm = " << patm << "\n";   // Atmospheric pressure [Pa]
+		std::cout << "   vpd = " << vpd << "\n";    // VPD [Pa]
+		std::cout << "   Rn = " << Rn << "\n";     // net radiation [w m-2]
+		std::cout << "   v_wind = " << v_wind << "\n"; // Wind speed [m s-1]
+		std::cout << "   viscosity_water = " << viscosity_water << "\n";  // [Pa s]
+		std::cout << "   density_water = " << density_water << "\n";    // [kg m-3]
+		std::cout << "   rho = " << rho << "\n";       // density of air [kg m-3]
+		std::cout << "   cp = " << cp << "\n";	      // specific heat capacity of moist air 
+		std::cout << "   gamma = " << gamma << "\n";     // psychrometric constant
+		std::cout << "   epsilon = " << epsilon << "\n";   // slope of saturation-pressure - temp curve
+		std::cout << "   lv = " << lv << "\n";        // latent heat of vaporization of water
 	}
 };
 
