fccdec0751548b13c72eb24528cd0cb85c50ec7a
Jaideep <jaideep777@gmail.com>
2024-02-23 11:01:21 +0100

    [hotfix_elv2pa_args] working demo vignettes

--- vignettes/rscript_for_beni.R
+++ vignettes/rscript_for_beni.R
@@ -14,6 +14,7 @@ rdark = 0.015;
 tc = 25;
 vwind = 3;
 netrad = ppfd/2
+pa = calc_patm(elv)
 
 par_cost = list(alpha=0.1, gamma=1)
 
@@ -31,12 +32,12 @@ par_plant_acclim = list(conductivity=3e-17,
                         psi50=-2, 
                         b=2)
 
-acc1 = rphydro_analytical(tc, tc, ppfd, netrad, vpd_acc, co2, elv, fapar, kphio, 0, rdark, vwind, par_plant_acclim, par_cost, options)
+acc1 = rphydro_analytical(tc, tc, ppfd, netrad, vpd_acc, co2, pa, fapar, kphio, 0, rdark, vwind, par_plant_acclim, par_cost, options)
 
 dat1 = list(vpd = exp(seq(log(100),log(5000),length.out=50)),
            psi_soil = c(0, -2.5)) %>%
   cross_df() %>% 
-  mutate(dat = purrr::map2(.x=psi_soil, .y=vpd, .f = ~rphydro_instantaneous_analytical(acc1$vcmax25, acc1$jmax25, tc, tc, ppfd, netrad, .y, co2, elv, fapar, kphio, .x, rdark, vwind, par_plant_acclim, par_cost, options))) %>% 
+  mutate(dat = purrr::map2(.x=psi_soil, .y=vpd, .f = ~rphydro_instantaneous_analytical(acc1$vcmax25, acc1$jmax25, tc, tc, ppfd, netrad, .y, co2, pa, fapar, kphio, .x, rdark, vwind, par_plant_acclim, par_cost, options))) %>% 
   unnest_wider(dat)
 
 
@@ -45,12 +46,12 @@ par_plant_no_acclim = list(conductivity=3e-17,
                            psi50=-5, 
                            b=2)
 
-acc2 = rphydro_analytical(tc, tc, ppfd, netrad, vpd_acc, co2, elv, fapar, kphio, 0, rdark, vwind, par_plant_no_acclim, par_cost, options)
+acc2 = rphydro_analytical(tc, tc, ppfd, netrad, vpd_acc, co2, pa, fapar, kphio, 0, rdark, vwind, par_plant_no_acclim, par_cost, options)
 
 dat2 = list(vpd = exp(seq(log(100),log(5000),length.out=50)),
            psi_soil = c(0, -2.5)) %>%
   cross_df() %>% 
-  mutate(dat = purrr::map2(.x=psi_soil, .y=vpd, .f = ~rphydro_instantaneous_analytical(acc2$vcmax25, acc2$jmax25, tc, tc, ppfd, netrad, .y, co2, elv, fapar, kphio, .x, rdark, vwind, par_plant_no_acclim, par_cost, options))) %>% 
+  mutate(dat = purrr::map2(.x=psi_soil, .y=vpd, .f = ~rphydro_instantaneous_analytical(acc2$vcmax25, acc2$jmax25, tc, tc, ppfd, netrad, .y, co2, pa, fapar, kphio, .x, rdark, vwind, par_plant_no_acclim, par_cost, options))) %>% 
   unnest_wider(dat)
 
 ## Make the desired plot
